4dc8311bf447cdf592b12650eaf607d610184d8f
slipfly <slipfly@gmail.com>
2023-05-24 23:21:25 +0300

    добавлены вспомогательные функции

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -48,4 +50,32 @@ const getRandomFloatList = (min, max, decimals, count) => {
   return newFloatList
 }
 
-export {getRandomInteger, getRandomNumsArray, getRandomFloat, getRandomFloatList, createList};
+const showSuccessMessage = () => {
+  console.log('success');
+};
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, getRandomNumsArray, getRandomFloat, getRandomFloatList,
+  createList, showSuccessMessage, showAlert, isEscapeKey};
