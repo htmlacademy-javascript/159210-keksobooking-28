5c923b03cb6328ddeeb9456816298f4d2d3d0ea6
slipfly <slipfly@gmail.com>
2023-05-24 18:34:02 +0300

    реорганизация кода

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import './util.js';
 import './data.js';
-import './render-blocks.js';
 import './form.js';
 import './map.js';
--- js/map.js
+++ js/map.js
@@ -1,5 +1,12 @@
 import { offersList } from './data.js';
-import { PLACE_TYPES } from './render-blocks.js';
+
+const PLACE_TYPES = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
 
 const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const COPYRIGHT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
