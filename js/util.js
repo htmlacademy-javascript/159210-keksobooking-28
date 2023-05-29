5bda887adeb8c23b1be47ba16f8e9d482c048c88
slipfly <slipfly@gmail.com>
2023-05-29 23:54:16 +0300

    удалён лишний код

--- js/util.js
+++ js/util.js
@@ -1,77 +1,13 @@
 const ALERT_SHOW_TIME = 5000;
 
-const DEFAULT_RADIX = 10;
-
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-function getRandomFloat(min, max, decimals) {
-  const str = (Math.random() * (max - min) + min).toFixed(decimals);
-
-  return parseFloat(str);
-}
-
-const getRandomNumsArray = (count, maxNum = count) => {
-  const newList = [];
-  let newId = getRandomInteger(0, maxNum);
-
-  while (newList.length < count) {
-    if (newList.includes(newId)) {
-      newId = getRandomInteger(0, maxNum);
-    } else {
-      newList.push(newId);
-    }
-  }
-
-  return newList;
-};
-
-const createList = (source) => {
-  let listLength = getRandomInteger(1, source.length);
-
-  if (source.length < listLength) {
-    listLength = source.length;
-  }
-
-  const itemsNums = getRandomNumsArray(listLength, source.length - 1);
-  const newItemsList = [];
-  for (let i = 0; i < listLength; i++) {
-    newItemsList.push(source[itemsNums[i]]);
-  }
-  return newItemsList;
-};
-
-const getRandomFloatList = (min, max, decimals, count) => {
-  const newFloatList = [];
-  for (let i = 0; i < count; i++) {
-    const newFloat = getRandomFloat(min, max, decimals);
-    newFloatList.push(newFloat);
-  }
-  return newFloatList;
-};
+const alertContainer = document.querySelector('.alert-container');
 
 const showAlert = (message) => {
-  const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = '100';
-  alertContainer.style.position = 'absolute';
-  alertContainer.style.left = '0';
-  alertContainer.style.top = '0';
-  alertContainer.style.right = '0';
-  alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '20px';
-  alertContainer.style.textAlign = 'center';
-  alertContainer.style.backgroundColor = 'red';
-
   alertContainer.textContent = message;
 
-  document.body.append(alertContainer);
-
+  alertContainer.classList.remove('hidden');
   setTimeout(() => {
-    alertContainer.remove();
+    alertContainer.classList.add('hidden');
   }, ALERT_SHOW_TIME);
 };
 
@@ -86,17 +22,11 @@ function debounce(callback, timeoutDelay = 100) {
   };
 }
 
-function throttle(callback, delayBetweenFrames) {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
+const setInteractiveElementsAvailability =
+  (selector, container = document, state = true) => {
+    container.querySelectorAll(selector).forEach((element) => {
+      element.disabled = state;
+    });
   };
-}
 
-export {getRandomInteger, getRandomNumsArray, getRandomFloat, getRandomFloatList,
-  createList, showAlert, isEscapeKey, debounce, throttle, DEFAULT_RADIX};
+export { showAlert, isEscapeKey, debounce, setInteractiveElementsAvailability };
